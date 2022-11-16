f12464fa938d42b4f64854b2432ac39a85921c3f
Rick Sanchez <98305681+yakash222000@users.noreply.github.com>
2022-11-16 20:06:30 +0530

    Time: 3 ms (76.15%), Space: 43.1 MB (27.69%) - LeetHub

--- 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.java
+++ 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.java
@@ -1,19 +1,9 @@
 class Solution {
     public List<Integer> numOfBurgers(int tomatoSlices, int cheeseSlices) {
-        List<Integer> ans = new ArrayList<>();
-        if(tomatoSlices<cheeseSlices*2) return ans;
-        int first =0;
-            while(cheeseSlices>0 && tomatoSlices>cheeseSlices*2){
-                    tomatoSlices=tomatoSlices-4;
-                    cheeseSlices--;
-                    first++;
-            }
-            if(tomatoSlices==cheeseSlices*2){
-                    ans.add(first);
-                    ans.add(cheeseSlices);
-                    return ans;
-            }
-            return ans;
-            
+         int total=tomatoSlices-2*cheeseSlices;
+        if(total<0||total%2!=0||total/2>cheeseSlices){
+            return new ArrayList<>();
+        }
+        return Arrays.asList(total/2,cheeseSlices-total/2); 
     }
 }
