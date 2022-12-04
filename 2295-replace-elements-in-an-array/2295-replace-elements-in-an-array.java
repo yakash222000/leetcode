625d7be0f28027653f885e6891fe6dbe47ee8468
Rick Sanchez <98305681+yakash222000@users.noreply.github.com>
2022-12-05 00:44:49 +0530

    Time: 51 ms (87.72%), Space: 92.4 MB (94.74%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] arrayChange(int[] nums, int[][] operations) {
-        //int[] ans = new int[nums.length];
         HashMap<Integer,Integer> map =  new HashMap<>();
         for(int i=0;i<nums.length;i++){
                 map.put(nums[i],i);
