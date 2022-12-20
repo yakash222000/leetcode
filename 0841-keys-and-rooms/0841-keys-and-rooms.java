26e325a78b20dd30b6e968e25e78c3ba2f80f390
Rick Sanchez <98305681+yakash222000@users.noreply.github.com>
2022-12-20 17:14:43 +0530

    Time: 3 ms (57.13%), Space: 41.9 MB (93.08%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,21 @@
+class Solution {
+        HashSet<Integer> set = new HashSet<>();
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {        
+        boolean[] arr = new boolean[rooms.size()];
+        set.add(0);
+        help(rooms,set,arr,0);
+        System.out.println(set);
+        return set.size()==rooms.size();
+    }
+    void help(List<List<Integer>> rooms,HashSet<Integer> set ,boolean[] arr ,int ind ){
+           if(arr[ind] || set.size()==rooms.size()) return;
+           arr[ind]=true;
+           for(int i=0;i<rooms.get(ind).size();i++){
+                   set.add(rooms.get(ind).get(i));
+           }
+           for(int i=0;i<rooms.get(ind).size();i++){
+                   help(rooms,set,arr,(rooms.get(ind).get(i)));
+           }       
+    }
+    
+}
