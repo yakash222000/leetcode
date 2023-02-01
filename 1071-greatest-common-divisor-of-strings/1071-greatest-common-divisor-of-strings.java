7bbefa6c9c8d6433ad2918f21fe11e3013954bbc
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-02-01 07:55:31 +0530

    Time: 44 ms (7.48%), Space: 42.4 MB (49.21%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -4,7 +4,6 @@ class Solution {
         
         for(int i=0;i<Math.min(str1.length(),str2.length());i++){
                 String curr=str1.substring(0,i+1);
-                //System.out.println(curr);
                 boolean t = true;
                 for(int j=0;j<Math.max(str1.length(),str2.length());j+=i+1){
                         if(j+i<str1.length() && !curr.equals(str1.substring(j,j+i+1))){
@@ -16,13 +15,14 @@ class Solution {
                                 break;
                         }
                 }
-                if(str2.length()%curr.length()!=0 || str1.length()%curr.length()!=0 ) t=false;
+                if(str2.length()%curr.length()!=0 ||
+                   str1.length()%curr.length()!=0 ) 
+                        t=false;
                 if(t){
                         ans.setLength(0);
                         ans.append(curr);
-                        //System.out.println(ans);
                 }
         }
-            return ans.toString();
+        return ans.toString();
     }
 }
