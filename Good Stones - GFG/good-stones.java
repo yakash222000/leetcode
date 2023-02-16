9686b96904c2055b5a4113b33e9595a058fdfa1f
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-02-16 15:31:24 +0530

    Added solution - LeetHub

---
+++ Good Stones - GFG/good-stones.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.goodStones(n, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int goodStones(int n, int[] arr) {
+        // code here
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            if(set.contains(i)) continue;
+            int j=i;
+            int temp=0;
+            HashSet<Integer> set1 = new HashSet<>();
+            while(j>=0 && j<n){
+                if(set1.contains(j) || set.contains(j)){
+                    temp=1;
+                    break;
+                }
+                set1.add(j);
+                j+=arr[j];
+            }
+            if(temp==1)set.addAll(set1);
+        }
+        //System.out.println(set);
+        return n-set.size();
+    }
+}
+        
