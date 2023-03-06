1d5059d5906445838f81cabd7d1cd37385535d27
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-03-06 20:03:29 +0530

    Time: 50 ms (34.56%), Space: 58.9 MB (77.88%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.java
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.java
@@ -1,18 +1,16 @@
 class Solution {
     public long minimalKSum(int[] nums, int k) {
-    Arrays.sort(nums);
-    Set<Integer> set = new HashSet<>();
-    long sum = 0;
-    
-    for (int num: nums) {
-        if (!set.contains(num) && num <= k) {
-            k++;
-            sum += num;        
-        }            
-        set.add(num);
-    }
-    
-    long res = (long)(1 + k) * k / 2 - sum;
-    return res;      
+        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<>();
+        long neg=0;
+        for(int i : nums){
+            if(!set.contains(i) && i<=k){
+                neg+=i;
+                k++;
+            }
+            set.add(i);
+        }
+        long ans = (long)(1+k)*k/2-neg;   
+        return ans;
     }
 }
