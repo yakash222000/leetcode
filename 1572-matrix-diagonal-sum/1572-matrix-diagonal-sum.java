92d58419bd531e58632a18da6c296bece86080d3
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-05-08 15:14:52 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (6.74%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,13 +1,13 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-            int res=0;
-       for(int i=0;i<mat.length;i++){
-               for(int j=0;j<mat.length;j++){
-                       if(i==j || i+j==mat.length-1)
-                res+=mat[i][j];
-               
-               }
-       } 
-            return  res;
+        int sum=0;
+        for(int i=0;i<mat.length;i++){
+            sum+=mat[i][i];
+            if(!(mat.length%2!=0 && i==mat.length/2)){
+                sum+=mat[i][mat.length-i-1];
+            }
+        }
+        return sum;
+        
     }
 }
