0053755412a7e624251df29aa588ca10ab7530bf
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-05-12 11:07:43 +0530

    Time: 7 ms (63.49%), Space: 93.9 MB (87.30%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long mostPoints(int[][] questions) {
+        long ans =0;
+        long[] dp = new long[questions.length];
+        dp[questions.length-1]=(long)questions[questions.length-1][0];
+        for(int i=questions.length-2;i>=0;i--){
+            if(questions[i][1]+i>=questions.length-1){
+                dp[i]=(long)questions[i][0];
+            }
+            else{
+                dp[i]=(long)questions[i][0];
+                dp[i]+=dp[questions[i][1]+1+i];   
+            }
+            dp[i]=Math.max(dp[i],dp[i+1]);
+        }
+        for(long i : dp){
+            ans=Math.max(i,ans);
+            //System.out.print(i+"  ");
+        }
+        return ans;
+    }
+}
