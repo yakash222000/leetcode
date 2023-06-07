adc7871f17607c9345f79a720784624324420fa3
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-06-07 21:58:34 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (72.22%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -3,11 +3,10 @@ class Solution {
         int ans=0;
         while(a!=0 ||b!=0 ||c!=0){
             int x=a&1,y=b&1,z=c&1;
-            System.out.println(x+" "+y+" "+z);
             if(x==0 && y==0 && z==1) ans++;
-            else if(x==1 && y==1 && z==0) ans+=2;
             else if((x==1 || y==1) && z==0){
-                ans++;
+                if(x==1 && y==1) ans+=2;
+                else ans++;
             }
             a=a>>1;
             b=b>>1;
