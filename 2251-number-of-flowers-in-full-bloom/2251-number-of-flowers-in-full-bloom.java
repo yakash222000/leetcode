365bb7be0860723533c19947459d472dfb7a720e
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-10-11 10:07:22 +0530

    Time: 102 ms (37.31%), Space: 79.4 MB (6.16%) - LeetHub

---
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] fullBloomFlowers(int[][] flowers, int[] persons) {
+        int n = persons.length;
+        int[] result = new int[n];
+
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for (int[] flower : flowers) {
+            treeMap.put(flower[0], treeMap.getOrDefault(flower[0], 0) + 1);
+            treeMap.put(flower[1] + 1, treeMap.getOrDefault(flower[1] + 1, 0) - 1);
+        }
+
+        TreeMap<Integer, Integer> sum = new TreeMap<>();
+        int prev = 0;
+        for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
+            prev += entry.getValue();
+            sum.put(entry.getKey(), prev);
+        }
+
+        for (int i = 0; i < n; i++) {
+            Map.Entry<Integer, Integer> entry = sum.floorEntry(persons[i]);
+            if (entry != null) result[i] = entry.getValue();
+        }
+        return result;
+    }
+}
