1edffc8888dc2cd9da09884200ef9692f2d367f9
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-11-17 13:11:37 +0530

    Time: 9 ms (95.14%), Space: 57.5 MB (23.21%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,11 +1,45 @@
 class Solution {
     public int minPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int ans = nums[0]+nums[nums.length-1];
-        for(int i=0;i<nums.length/2;i++){
-            ans=Math.max(ans,nums[i]+nums[nums.length-i-1]);
+        int maxSum = Integer.MIN_VALUE; // Variable to store the minimized maximum pair sum
+        int minNum = Integer.MAX_VALUE; // Variable to store the minimum number in the array
+        int maxNum = Integer.MIN_VALUE; // Variable to store the maximum number in the array
+
+        int frequency[] = new int[100001]; // Array to store the frequency of each number in the array
+
+        // Iterate through the array to populate frequency array and find min and max numbers
+        for (int num : nums) {
+            frequency[num]++;
+            minNum = Math.min(minNum, num);
+            maxNum = Math.max(maxNum, num);
         }
-        return ans;
+
+        // Initialize pointers for two numbers to form pairs
+        int low = minNum;
+        int high = maxNum;
+
+        // Iterate while low pointer is less than or equal to high pointer
+        while (low <= high) {
+            // If frequency of the number at low pointer is zero, move low pointer to the right
+            if (frequency[low] == 0) {
+                low++;
+            }
+            // If frequency of the number at high pointer is zero, move high pointer to the left
+            else if (frequency[high] == 0) {
+                high--;
+            }
+            // Both low and high pointers point to valid numbers
+            else {
+                // Calculate the sum of the pair at the current pointers
+                int currentPairSum = low + high;
+                // Update maxSum if the current pair sum is greater
+                maxSum = Math.max(maxSum, currentPairSum);
+                // Decrease the frequency of the numbers at low and high pointers
+                frequency[low]--;
+                frequency[high]--;
+            }
+        }
+
+        return maxSum;
     }
     
 }
