b03c6e5aaf1b0272b7c21e2928c2b09cb6d3b968
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2023-12-27 12:28:49 +0530

    Time: 8 ms (81.75%), Space: 60.6 MB (17.22%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,26 +1,22 @@
 class Solution {
-    
     public int minCost(String colors, int[] neededTime) {
-        int n=neededTime.length,res=0;
-        Stack<Pair<Character,Integer>> st = new Stack<>();
-        Pair<Character ,Integer> p = new Pair<>(colors.charAt(0),neededTime[0]);
-        st.push(p);
-        for(int i=1;i<n;i++){
-                p = new Pair<>(colors.charAt(i),neededTime[i]);
-                if(st.peek().getKey()==p.getKey()){
-                        if(st.peek().getValue() > p.getValue()){
-                                res+=p.getValue();
+        int n=neededTime.length,res=0,i=0,j=1;
+        while(j<n) {
+                if(colors.charAt(i)==colors.charAt(j)){
+                        if(neededTime[i]>neededTime[j]){
+                                res+=neededTime[j];
+                                j++;
                         }
                         else{
-                                res+=st.peek().getValue();
-                                st.pop();
-                                st.push(p);
+                                res+=neededTime[i];
+                                i=j;
+                                j++;
                         }
                 }
                 else{
-                        st.push(p);
+                        i=j;
+                        j++;
                 }
-                
         }
         return res;
     }
