12713915c815e2a933c8bcdf2142ab759d8a7213
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2024-03-13 07:58:13 +0530

    Time: 1 ms (77.54%), Space: 40.2 MB (94.39%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int pivotInteger(int n) {
+        for(int i=1;i<=n;i++){
+                if(i*(i+1)/2==n*(n+1)/2-(i-1)*i/2) return i;
+        }
+            return -1;
+    }
+}
