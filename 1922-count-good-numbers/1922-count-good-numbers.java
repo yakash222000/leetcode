85406f7f082d791731ff469096faa757f3e20349
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2024-08-23 12:43:14 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (65.37%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    int MOD = 1000000007;
+    
+    public long pow(long x, long y) {
+        if(y==0) return 1;
+        long temp = pow(x,y/2);
+        temp*=temp;
+        temp%=MOD;
+        if(y%2!=0) temp *= x;
+        temp%=MOD;
+        return temp;
+    }
+    
+    public int countGoodNumbers(long n) {
+        return (int)(pow(4,n/2)%MOD * pow(5,n/2+n%2)%MOD);
+    }
+    
+}
