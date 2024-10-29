ec2bfc4b53bc9c642ed29c32ba90cb9b49927585
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2024-10-29 13:18:53 +0530

    Time: 6 ms (80.77%), Space: 56.3 MB (60.15%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
@@ -5,21 +5,21 @@ class Solution {
         int max = 0,last=0;
         for(int i=1;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(j!=n-1){
-                    if(grid[j][i]>grid[j+1][i-1]) {
-                        dp[j][i] = Math.max(dp[j][i],dp[j+1][i-1]+1);
-                    }
+                // Lower left cell check
+                if(j!=n-1 && grid[j][i]>grid[j+1][i-1]){
+                    dp[j][i] = Math.max(dp[j][i],dp[j+1][i-1]+1);
                 }
+                // Left cell check
                 if(grid[j][i]>grid[j][i-1]) {
                     dp[j][i] = Math.max(dp[j][i],dp[j][i-1]+1);
                 }
-                if(j!=0){
-                    if(grid[j][i]>grid[j-1][i-1]) {
-                        dp[j][i] = Math.max(dp[j][i],dp[j-1][i-1]+1);
-                    }
+                // Upper left cell check
+                if(j!=0 && grid[j][i]>grid[j-1][i-1]){
+                    dp[j][i] = Math.max(dp[j][i],dp[j-1][i-1]+1);
                 }   
                 max = Math.max(max, dp[j][i]);
             }
+            // If moves are not incremented from the last column return 
             if(max == last) return max;
             last = max;
         }
