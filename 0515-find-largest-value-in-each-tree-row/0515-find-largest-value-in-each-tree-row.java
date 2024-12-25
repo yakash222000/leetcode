44e95afea0019a046cd2b1fd74edfe1ad04cc197
Akash Singh Yadav <98305681+yakash222000@users.noreply.github.com>
2024-12-26 02:41:23 +0530

    Time: 1 ms (94.7%), Space: 45.8 MB (12.41%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -1,36 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    private List<Integer> res;
     public List<Integer> largestValues(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if(root==null) return res;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        int max=Integer.MIN_VALUE;
-        while(!q.isEmpty()){
-                max=Integer.MIN_VALUE;
-                int n=q.size();
-                for(int i=0;i<n;i++){
-                        TreeNode node =q.remove();
-                        if(node.left!=null)q.add(node.left);
-                        if(node.right!=null)q.add(node.right);
-                        max=Math.max(max,node.val);
-                }
-                res.add(max);
-        }
+        res = new ArrayList<>();
+        dfs(root, 0);
+
         return res;
+    }
+
+    private void dfs(TreeNode root, int level) {
+        if (root != null) {
+            int val = root.val;
+            
+            if (res.size() == level) 
+                res.add(val);
+            else 
+                res.set(level, Math.max(res.get(level), val));
+            
+            dfs(root.left, level + 1);
+            dfs(root.right, level + 1);
+        }
     }
 }
